506e5776f7e3d4e5e98e23ecb1bfd95c090c0826
Thomas A <curioustommy@protonmail.com>
2022-05-09 17:21:58 -0700

    Manually Add ImGuiDockNodeFlagsPrivate_ Items To Generated ImGuiDockNodeFlags

--- src/ImGuiSharp/Generated/ImGuiDockNodeFlags.gen.cs
+++ src/ImGuiSharp/Generated/ImGuiDockNodeFlags.gen.cs
@@ -11,5 +11,21 @@ namespace ImGuiSharp
         NoSplit = 16,
         NoResize = 32,
         AutoHideTabBar = 64,
+
+        // Manually added entries from ImGuiDockNodeFlagsPrivate_
+        DockSpace = 1024 /* 1 << 10 */,
+        CentralNode = 2048 /* 1 << 11 */,
+        NoTabBar = 4096 /* 1 << 12 */,
+        HiddenTabBar = 8192 /* 1 << 13 */,
+        NoWindowMenuButton = 16384 /* 1 << 14 */,
+        NoCloseButton = 32768 /* 1 << 15 */,
+        NoDocking = 65536 /* 1 << 16 */,
+        NoDockingSplitMe = 131072 /* 1 << 17 */,
+        NoDockingSplitOther = 262144 /* 1 << 18 */,
+        NoDockingOverMe = 524288 /* 1 << 19 */,
+        NoDockingOverOther = 1048576 /* 1 << 20 */,
+        NoDockingOverEmpty = 2097152 /* 1 << 21 */,
+        NoResizeX = 4194304 /* 1 << 22 */,
+        NoResizeY = 8388608 /* 1 << 23 */,
     }
 }
